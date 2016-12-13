64a836003514cb6dc2936d007ca1164c2fd7bab7
danelliottpalmer <danelliottpalmer@gmail.com>
2016-12-13 17:15:28 +0000

    refactor(State): update state to fire events when values change

--- server/State.js
+++ server/State.js
@@ -1,17 +1,36 @@
 'use strict';
 
-function State(){
-	this._ = new Map();
+const EventEmitter2 = require('eventemitter2');
+const isEqual = require('lodash.isequal');
+
+class State extends EventEmitter2 {
+
+    constructor(){
+        super({
+            'newListener': false,
+            'verboseMemoryLeak': true,
+            'wildcard': true
+        });
+
+        this._ = new Map();
+    }
+
+    get(key){
+        return this._.get(key);
+    }
+
+    has(key){
+        return this._.has(key);
+    }
+
+    set(key, value){
+        if(this.has(key) && !isEqual(this.get(key), value)){
+            this.emit('change', key, value, this.get(key));
+        }
+        return this._.set(key, value);
+    }
+
 }
-State.prototype.get = function(key){
-	return this._.get(key);
-};
-State.prototype.has = function(key){
-	return this._.has(key);
-};
-State.prototype.set = function(key, value){
-	return this._.set(key, value);
-};
 
 const state = new State();
 state.set('queue', []);
